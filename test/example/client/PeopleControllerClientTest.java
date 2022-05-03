93d86746e21483b92d172766f650b7f79d9428af
V <s19159@pjwstk.edu.pl>
2022-05-03 13:01:53 +0200

    init

--- test/example/client/PeopleControllerClientTest.java
+++ test/example/client/PeopleControllerClientTest.java
@@ -1,9 +1,12 @@
 package example.client;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 
@@ -19,15 +22,38 @@ class PeopleControllerClientTest {
 
     @Test
     void getPeople() {
-        List<Person> people = peopleController.getPeople(new Filter());
+        Filter filter = new Filter();
+        filter.setBirthdate( new XMLGregorianCalendarImpl(new GregorianCalendar(2000, Calendar.JUNE, 3)));
+        filter.setSurname("Doe");
+        List<Person> people = peopleController.getPeople(filter);
+        Assertions.assertEquals(1, people.size());
+        Assertions.assertEquals("John", people.get(0).firstName);
+        Assertions.assertEquals("Doe", people.get(0).surname);
+        Assertions.assertEquals(new XMLGregorianCalendarImpl(new GregorianCalendar(2000, Calendar.JUNE, 3)), people.get(0).birthdate);
     }
     @Test
     void getPeopleBySurname() {
-        List<Person> people = peopleController.getPeople(new Filter());
+        Filter filter = new Filter();
+        filter.setSurname("Doe");
+        List<Person> people = peopleController.getPeople(filter);
+        Assertions.assertEquals(2, people.size());
+        Assertions.assertEquals("John", people.get(0).firstName);
+        Assertions.assertEquals("Doe", people.get(0).surname);
+        Assertions.assertEquals(new XMLGregorianCalendarImpl(new GregorianCalendar(2000, Calendar.JUNE, 3)), people.get(0).birthdate);
+        Assertions.assertEquals("Michael", people.get(1).firstName);
+        Assertions.assertEquals("Doe", people.get(1).surname);
+        Assertions.assertEquals(new XMLGregorianCalendarImpl(new GregorianCalendar(1999, Calendar.DECEMBER, 9)), people.get(1).birthdate);
+
     }
     @Test
     void getPeopleByBirthdate() {
-        List<Person> people = peopleController.getPeople(new Filter());
+        Filter filter = new Filter();
+        filter.setBirthdate( new XMLGregorianCalendarImpl(new GregorianCalendar(2000, Calendar.JUNE, 3)));
+        List<Person> people = peopleController.getPeople(filter);
+        Assertions.assertEquals(1, people.size());
+        Assertions.assertEquals("John", people.get(0).firstName);
+        Assertions.assertEquals("Doe", people.get(0).surname);
+        Assertions.assertEquals(new XMLGregorianCalendarImpl(new GregorianCalendar(2000, Calendar.JUNE, 3)), people.get(0).birthdate);
     }
     @Test
     void getAllPeople() {
